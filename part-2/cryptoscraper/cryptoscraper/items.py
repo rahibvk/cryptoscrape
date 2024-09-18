71a0ba61fde8a16a248356eafafa963ea4844521
rahibvk <91156242+rahibvk@users.noreply.github.com>
2024-09-18 23:51:14 +0300

    Update items.py

--- part-2/cryptoscraper/cryptoscraper/items.py
+++ part-2/cryptoscraper/cryptoscraper/items.py
@@ -10,3 +10,8 @@ class CryptoscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class CryptoItem(scrapy.item):
+    url = scrapy.Item()
+    value = scrapy.item()
